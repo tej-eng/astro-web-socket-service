b4de42d9b4eac8790c828bb6b60eacf04479787f
tejpal singh <tej@dhwaniastro.com>
2026-04-21 11:23:26 +0530

    change logs

--- socketdata/index.js
+++ socketdata/index.js
@@ -57,6 +57,7 @@ const redisHandlers = (io) => ({
    
     const parseData = typeof data === "string" ? JSON.parse(data) : data;
     if (parseData.sender == "user") {
+      console.log("[messages handler] Emitting to room (User)aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa:");  
       io.to(parseData.room_id).emit("receive_message", parseData);
     } else if (parseData.sender === "Astrologer") {
       console.log("[messages handler] Emitting to room (Astrologer):", parseData.room_id);
